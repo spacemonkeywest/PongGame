f0ffdbc4cdb5a611a514da6c2dcf3f0226dc707e
spacemonkeywest <caldaco12@gmail.com>
2024-08-10 21:53:01 -0700

    added window and rectangle

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-using namespace sf;
-
 int main() {
-	RenderWindow window(VideoMode(800, 800), "New Window");
+	sf::RenderWindow window(sf::VideoMode(800, 800), "New Window");
+
+	sf::RectangleShape rectangle;
+	rectangle.setSize(sf::Vector2f(100, 50));
+	rectangle.setFillColor(sf::Color::White);
+	rectangle.setOutlineColor(sf::Color::White);
+	rectangle.setOutlineThickness(5);
+	rectangle.setPosition(10, 20);
+
 	while (window.isOpen()) {
-		Event event;
+		sf::Event event;
 		while(window.pollEvent(event)) {
-			if (event.type == Event::Closed) {
+			if (event.type == sf::Event::Closed) {
 				window.close();
 			}
-		window.clear();
-		window.display();
 		}
+		window.clear();
+		std::cout << "Rectangle position: " << rectangle.getPosition().x 		i<< ", " << rectangle.getPosition().y << std::endl;
+
+		window.draw(rectangle);
+		window.display();
 	}
 	return EXIT_SUCCESS;
 }
